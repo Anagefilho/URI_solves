233c12738e3e9d303cfa989a4356e0c2bce89456
ANAGE CALIXTO MUNDIM FILHO <anagefilho@hotmail.com>
2020-02-26 18:18:19 -0300

    Create 1067.cpp

---
+++ Sequential/1067.cpp
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+    #include <iostream>
+    using namespace std;
+    int main (){
+    	int n=0, i;
+    	scanf("%d", &n);
+    	for (i=1; i<=n; i=i+2){
+     
+    	 printf("%d\n", i);
+     
+    	}		
+    	return 0;
+    }
